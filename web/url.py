2a0ed08aa82ead12551046d8d1f1f6d938ac2742
crosscent <crosscent@gmail.com>
2014-09-18 10:28:22 -0700

    fixed web\views.py
    imported render_to_response

--- web/url.py
+++ web/url.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from vendors.views import registration
 
 urlpatterns = patterns('',
-	url(r'^$', 'web.views.index'),
+    url(r'^$', 'web.views.index'),
     url(r'^registration/$', 'vendors.views.registration', name='registration'),
     url(r'^login/$', 'vendors.views.user_login', name='login'),
     url(r'^logout/$', 'vendors.views.user_logout', name='logout'),
